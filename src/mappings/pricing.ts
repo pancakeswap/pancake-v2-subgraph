8b6b263a92a82b23f7f1f9ce26b837ae2f7dd6e6
ChefKai <74599990+ChefKai@users.noreply.github.com>
2021-02-25 10:33:53 +0100

    chore: Add tracked tokens (#4)

--- src/mappings/pricing.ts
+++ src/mappings/pricing.ts
@@ -45,6 +45,13 @@ let WHITELIST: string[] = [
   '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c', // WBNB
   '0xe9e7cea3dedca5984780bafc599bd69add087d56', // BUSD
   '0x55d398326f99059ff775485246999027b3197955', // USDT
+  '0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d', // USDC
+  '0x23396cf899ca06c4472205fc903bdb4de249d6fc', // UST
+  '0x1af3f329e8be154074d8769d1ffa4ee058b1dbc3', // DAI
+  '0x4bd17003473389a42daf6a0a729f6fdb328bbbd7', // VAI
+  '0x7130d2a12b9bcbfae4f2634d864a1ee1ce3ead9c', // BTCB
+  '0x2170ed0880ac9a755fd29b2688956bd959f933f8', // WETH
+  '0x250632378e573c6be1ac2f97fcdf00515d0aa91b', // BETH
 ]
 
 // minimum liquidity for price to get tracked
