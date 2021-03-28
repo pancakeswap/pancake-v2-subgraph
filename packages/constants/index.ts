84de81b220fb9e5e3155b859e1742d6cb3808159
@codingsh <codingsh@pm.me>
2021-03-28 10:02:24 -0300

    feat(subgraph/exchange): add exchange subgraph and cleaning old files

--- packages/constants/index.ts
+++ packages/constants/index.ts
@@ -20,35 +20,35 @@ export const BIG_INT_ZERO = BigInt.fromI32(0)
 
 export const LOCKUP_POOL_NUMBER = BigInt.fromI32(29)
 
-export const FACTORY_ADDRESS = Address.fromString('0xc0aee478e3658e2610c5f7a4a2e1777ce9e4f2ac')
+export const FACTORY_ADDRESS = Address.fromString('0xBCfCcbde45cE874adCB698cC183deBcF17952812')
 
-export const LOCKUP_BLOCK_NUMBER = BigInt.fromI32(10959148)
+export const LOCKUP_BLOCK_NUMBER = BigInt.fromI32(586851)
 
-export const MASTER_CHEF_ADDRESS = Address.fromString('0xc2edad668740f1aa35e4d8f227fb8e17dca888cd')
+export const MASTER_CHEF_ADDRESS = Address.fromString('0x73feaa1eE314F8c655E354234017bE2193C9E24E')
 
-export const SUSHI_BAR_ADDRESS = Address.fromString('0x8798249c2e607446efb7ad49ec89dd1865ff4272')
+export const SUSHI_BAR_ADDRESS = Address.fromString('')
 
-export const SUSHI_MAKER_ADDRESS = Address.fromString('0xE11fc0B43ab98Eb91e9836129d1ee7c3Bc95df50')
+export const SUSHI_MAKER_ADDRESS = Address.fromString('')
 
-export const SUSHI_TOKEN_ADDRESS = Address.fromString('0x6b3595068778dd592e39a122f4f5a5cf09c90fe2')
+export const SUSHI_TOKEN_ADDRESS = Address.fromString('0x0e09fabb73bd3ade0a17ecc321fd13a19e81ce82')
 
-export const SUSHI_USDT_PAIR_ADDRESS = Address.fromString('0x680a025da7b1be2c204d7745e809919bce074026')
+export const SUSHI_USDT_PAIR_ADDRESS = Address.fromString('')
 
-export const XSUSHI_USDC_PAIR_ADDRESS = Address.fromString('0xd597924b16cc1904d808285bc9044fd51ceeead7')
+export const XSUSHI_USDC_PAIR_ADDRESS = Address.fromString('')
 
-export const XSUSHI_WETH_PAIR_ADDRESS = Address.fromString('0x36e2fcccc59e5747ff63a03ea2e5c0c2c14911e7')
+export const XSUSHI_WETH_PAIR_ADDRESS = Address.fromString('')
 
 export const SUSHI_DISTRIBUTOR_ADDRESS = Address.fromString('')
 
 export const NULL_CALL_RESULT_VALUE = '0x0000000000000000000000000000000000000000000000000000000000000001'
 
-export const USDC_WETH_PAIR = '0x397ff1542f962076d0bfe58ea045ffa2d347aca0'
+export const USDC_WETH_PAIR = ''
 
-export const DAI_WETH_PAIR = '0xc3d03e4f041fd4cd388c549ee2a29a9e5075882f'
+export const DAI_WETH_PAIR = ''
 
-export const USDT_WETH_PAIR = '0x06da0fd433c1a5d7a4faa01111c044910a184553'
+export const USDT_WETH_PAIR = ''
 
-export const SUSHI_USDT_PAIR = '0x680a025da7b1be2c204d7745e809919bce074026'
+export const SUSHI_USDT_PAIR = ''
 
 export const WHITELIST: string[] = [
   '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2', // WETH
@@ -75,20 +75,20 @@ export const MINIMUM_USD_THRESHOLD_NEW_PAIRS = BigDecimal.fromString('0')
 // minimum liquidity for price to get tracked
 export const MINIMUM_LIQUIDITY_THRESHOLD_ETH = BigDecimal.fromString('0')
 
-export const WETH_ADDRESS = Address.fromString('0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2')
+export const WETH_ADDRESS = Address.fromString('')
 
-export const SUSHISWAP_WETH_USDT_PAIR_ADDRESS = Address.fromString('0x06da0fd433c1a5d7a4faa01111c044910a184553')
+export const SUSHISWAP_WETH_USDT_PAIR_ADDRESS = Address.fromString('')
 
-export const USDT_ADDRESS = Address.fromString('0xdac17f958d2ee523a2206206994597c13d831ec7')
+export const USDT_ADDRESS = Address.fromString('')
 
-export const MASTER_CHEF_START_BLOCK = BigInt.fromI32(10750000)
+export const MASTER_CHEF_START_BLOCK = BigInt.fromI32(699498)
 
-export const UNISWAP_FACTORY_ADDRESS = Address.fromString('0x5c69bee701ef814a2b6a3edd4b1652cb9cc5aa6f')
+export const UNISWAP_FACTORY_ADDRESS = Address.fromString('')
 
 export const UNISWAP_SUSHI_ETH_PAIR_FIRST_LIQUDITY_BLOCK = BigInt.fromI32(10750005)
 
-export const UNISWAP_WETH_USDT_PAIR_ADDRESS = Address.fromString('0x0d4a11d5eeaac28ec3f61d100daf4d40471f1852')
+export const UNISWAP_WETH_USDT_PAIR_ADDRESS = Address.fromString('')
 
-export const UNISWAP_SUSHI_ETH_PAIR_ADDRESS = Address.fromString('0xce84867c3c02b05dc570d0135103d3fb9cc19433')
+export const UNISWAP_SUSHI_ETH_PAIR_ADDRESS = Address.fromString('')
 
-export const UNISWAP_SUSHI_USDT_PAIR_ADDRESS = Address.fromString('0xe3ffab89e53422f468be955e7011932efe80aa26')
+export const UNISWAP_SUSHI_USDT_PAIR_ADDRESS = Address.fromString('')
